1a7139dae0b9a48c73377851cdbe054a5ff3f650
elparo290 <parovelfilippo1@gmail.com>
2022-01-14 15:13:21 +0100

    creazione database

---
+++ pages/Database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE ProggettoScentifico;
+
+SHOW DATABASES;
+ USE ProggettoScentifico;
+ CREATE TABLE Private(
+    name text,
+    surname text,
+    mail varchar,
+    password varchar,
+    accountingAdress varchar,
+    telephone int,
+    id varchar;
+)
