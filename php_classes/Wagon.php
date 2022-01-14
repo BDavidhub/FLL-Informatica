fb567c5ca3fcff80b4038f40706d3edbc537ee39
elparo290 <parovelfilippo1@gmail.com>
2022-01-14 11:50:01 +0100

    creazione struttura di quasi tutte le classi

---
+++ php_classes/Wagon.php
@@ -0,0 +1,35 @@
+<?php
+    class Wagon extends Utility{
+        private $arrayOfBox;
+        private $capacity;
+
+        public function Wagon($arrayOfBox, $capacity)
+        {
+            $this->arrayOfBox=$arrayOfBox;
+            $this->capacity=$capacity;
+            $this->Utility("wagon");
+        }
+
+        public function getArrayOfBox() {
+            return $this->arrayOfBox;
+        }
+        
+        public function setArrayOfBox($arrayOfBox) {
+             $this->arrayOfBox = $arrayOfBox;
+        }
+        
+        public function getCapacity() {
+            return $this->capacity;
+        }
+        
+        public function setCapacity($capacity) {
+             $this->capacity = $capacity;
+        }
+
+        public function calcolaSpazio()
+        {
+            return 1;
+        }
+
+    }
+?>
