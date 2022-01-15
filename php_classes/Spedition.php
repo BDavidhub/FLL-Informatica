2f2a6f5987e4a0e3c7eb972fa8ff86d1d925ef6e
elparo290 <parovelfilippo1@gmail.com>
2022-01-15 08:19:49 +0100

    Update Spedition.php

--- php_classes/Spedition.php
+++ php_classes/Spedition.php
@@ -3,16 +3,14 @@
     {
         private $arrayOfPath;
         private $treno;
-        private $timeStamp;
-        private $partenza;
+        private $timeStampPartenza;
         
-        public function Spedition($arrayOfPath, $treno, $timeStamp, $partenza)
+        public function Spedition($arrayOfPath, $treno, $timeStampPartenza)
         {
             $this->Utility("spedition");
             $this->arrayDiPath=$arrayOfPath;
             $this->treno=$treno;
-            $this->timeStamp=$timeStamp;
-            $this->partenza=$partenza;
+            $this->timeStamp=$timeStampPartenza;
         }
 
         public function getArrayOfPath()
@@ -35,24 +33,14 @@
             $this->treno=$treno;
         }
 
-        public function getTimeStamp()
+        public function getTimeStampPartenza()
         {
-            return $this->timeStamp;
+            return $this->timeStampPartenza;
         }
 
-        public function setTimeStamp($timeStamp)
+        public function setTimeStampPartenza($timeStampPartenza)
         {
-            $this->timeStamp=$timeStamp;
-        }
-
-        public function getPartenza()
-        {
-            return $this->partenza;
-        }
-
-        public function setPartenza($partenza)
-        {
-            $this->partenza=$partenza;
+            $this->timeStampPartenza=$timeStampPartenza;
         }
 
     }
