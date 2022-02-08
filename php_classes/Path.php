cd642a10c301319121dc3d73bf5c838878277d06
elparo290 <parovelfilippo1@gmail.com>
2022-02-08 14:27:23 +0100

    Prima prova path

--- php_classes/Path.php
+++ php_classes/Path.php
@@ -17,4 +17,30 @@ class Path
     {
         $this->arrayOfHub = $arrayOfHub;
     }
+
+    public function getWagonsByTrain($train,$hub)
+    {
+        if($train->getPreviousHub($this)!=false ) $in = $train->getPreviousHub($this)->getTrainInOutConfig($train);
+        else $in=null;
+        $out = $this->getTrainInOutConfig($train,$hub);
+       
+        return array($in,$out);
+    }
+
+    public function getTrainInOutConfig($train,$hub){
+        $wagons = array();
+         if (!in_array($hub, $train->getHubs())) return null;
+         foreach($train->getWagons() as $key => $wagon){
+             if(in_array($hub,$wagon->getHubs()) && $wagon->getHubArrive() != $this){
+                 $wagons[] = $wagon;
+             }
+         }
+         $wagons=$train->getOrdinatedWagons($hub,$wagons);
+         if(count($wagons)==0) return null;
+         return $wagons;
+     }
+
+    //  Spostato dalla classe hub le due funzioni soprastanti
+    //  aspetto commento per capire se modificare anche tutto il resto
+
 }
