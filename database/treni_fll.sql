0a7c0c4489c6a4deb48af1d50344bbaff320262f
Eliminatore 303 <davideb303@libero.it>
2022-03-16 14:31:00 +0100

    finisched the problem with the primary keys of the tables

--- database/treni_fll.sql
+++ database/treni_fll.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mar 10, 2022 alle 16:39
+-- Creato il: Mar 16, 2022 alle 14:27
 -- Versione del server: 10.4.22-MariaDB
 -- Versione PHP: 8.0.13
 
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `boxes` (
-  `ID-B` int(10) UNSIGNED NOT NULL,
+  `ID_B` int(10) UNSIGNED NOT NULL,
   `Size` int(5) NOT NULL,
-  `ID-U` int(10) UNSIGNED NOT NULL,
-  `ID-W` int(10) UNSIGNED NOT NULL
+  `ID_U` int(10) UNSIGNED NOT NULL,
+  `ID_W` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -41,7 +41,7 @@ CREATE TABLE `boxes` (
 --
 
 CREATE TABLE `entersprises` (
-  `ID-U` int(10) UNSIGNED NOT NULL,
+  `ID_U` int(10) UNSIGNED NOT NULL,
   `VAT` char(11) NOT NULL,
   `CompanyName` varchar(50) NOT NULL,
   `AccountingAddressE` varchar(50) NOT NULL
@@ -54,8 +54,8 @@ CREATE TABLE `entersprises` (
 --
 
 CREATE TABLE `has` (
-  `ID-T` int(10) UNSIGNED NOT NULL,
-  `ID-W` int(10) UNSIGNED NOT NULL,
+  `ID_T` int(10) UNSIGNED NOT NULL,
+  `ID_W` int(10) UNSIGNED NOT NULL,
   `Entra` int(10) UNSIGNED NOT NULL,
   `Esce` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -67,7 +67,7 @@ CREATE TABLE `has` (
 --
 
 CREATE TABLE `hubs` (
-  `ID-H` int(10) UNSIGNED NOT NULL,
+  `ID_H` int(10) UNSIGNED NOT NULL,
   `NameH` varchar(50) NOT NULL,
   `CapacityH` int(5) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -79,8 +79,8 @@ CREATE TABLE `hubs` (
 --
 
 CREATE TABLE `passes_by` (
-  `ID-T` int(10) UNSIGNED NOT NULL,
-  `ID-H` int(10) UNSIGNED NOT NULL,
+  `ID_T` int(10) UNSIGNED NOT NULL,
+  `ID_H` int(10) UNSIGNED NOT NULL,
   `PassageNumber` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -91,7 +91,7 @@ CREATE TABLE `passes_by` (
 --
 
 CREATE TABLE `privates` (
-  `ID-U` int(10) UNSIGNED NOT NULL,
+  `ID_U` int(10) UNSIGNED NOT NULL,
   `NameP` varchar(50) NOT NULL,
   `SurnameP` varchar(50) NOT NULL,
   `AccountingAddressP` varchar(50) NOT NULL
@@ -104,7 +104,7 @@ CREATE TABLE `privates` (
 --
 
 CREATE TABLE `trains` (
-  `ID-T` int(10) UNSIGNED NOT NULL,
+  `ID_T` int(10) UNSIGNED NOT NULL,
   `LimitT` int(5) UNSIGNED NOT NULL,
   `DataTimeDeparture` datetime NOT NULL,
   `SerialNumberT` int(10) DEFAULT NULL
@@ -117,7 +117,7 @@ CREATE TABLE `trains` (
 --
 
 CREATE TABLE `users` (
-  `ID-U` int(10) UNSIGNED NOT NULL,
+  `ID_U` int(10) UNSIGNED NOT NULL,
   `Mail` varchar(50) NOT NULL,
   `Telephone` char(15) DEFAULT NULL,
   `Password` varchar(50) NOT NULL
@@ -130,8 +130,8 @@ CREATE TABLE `users` (
 --
 
 CREATE TABLE `usrhubs` (
-  `ID-U` int(10) UNSIGNED NOT NULL,
-  `ID-H` int(10) UNSIGNED NOT NULL
+  `ID_U` int(10) UNSIGNED NOT NULL,
+  `ID_H` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -141,7 +141,7 @@ CREATE TABLE `usrhubs` (
 --
 
 CREATE TABLE `wagons` (
-  `ID-W` int(10) UNSIGNED NOT NULL,
+  `ID_W` int(10) UNSIGNED NOT NULL,
   `CapacityW` int(5) UNSIGNED NOT NULL,
   `SerialNumberW` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -154,70 +154,70 @@ CREATE TABLE `wagons` (
 -- Indici per le tabelle `boxes`
 --
 ALTER TABLE `boxes`
-  ADD PRIMARY KEY (`ID-B`),
-  ADD KEY `Box_Use` (`ID-U`),
-  ADD KEY `Box_Wag` (`ID-W`);
+  ADD PRIMARY KEY (`ID_B`),
+  ADD KEY `Box_Use` (`ID_U`),
+  ADD KEY `Box_Wag` (`ID_W`);
 
 --
 -- Indici per le tabelle `entersprises`
 --
 ALTER TABLE `entersprises`
-  ADD PRIMARY KEY (`ID-U`),
+  ADD PRIMARY KEY (`ID_U`),
   ADD UNIQUE KEY `UNIQUE` (`CompanyName`,`AccountingAddressE`);
 
 --
 -- Indici per le tabelle `has`
 --
 ALTER TABLE `has`
-  ADD PRIMARY KEY (`ID-T`,`ID-W`),
-  ADD KEY `Has_Wag` (`ID-W`);
+  ADD PRIMARY KEY (`ID_T`,`ID_W`),
+  ADD KEY `Has_Wag` (`ID_W`);
 
 --
 -- Indici per le tabelle `hubs`
 --
 ALTER TABLE `hubs`
-  ADD PRIMARY KEY (`ID-H`),
+  ADD PRIMARY KEY (`ID_H`),
   ADD UNIQUE KEY `UNIQUE` (`NameH`);
 
 --
 -- Indici per le tabelle `passes_by`
 --
 ALTER TABLE `passes_by`
-  ADD PRIMARY KEY (`ID-T`,`ID-H`),
-  ADD KEY `Pas_Hub` (`ID-H`);
+  ADD PRIMARY KEY (`ID_T`,`ID_H`),
+  ADD KEY `Pas_Hub` (`ID_H`);
 
 --
 -- Indici per le tabelle `privates`
 --
 ALTER TABLE `privates`
-  ADD PRIMARY KEY (`ID-U`),
+  ADD PRIMARY KEY (`ID_U`),
   ADD UNIQUE KEY `UNIQUE` (`AccountingAddressP`);
 
 --
 -- Indici per le tabelle `trains`
 --
 ALTER TABLE `trains`
-  ADD PRIMARY KEY (`ID-T`);
+  ADD PRIMARY KEY (`ID_T`);
 
 --
 -- Indici per le tabelle `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID-U`),
+  ADD PRIMARY KEY (`ID_U`),
   ADD UNIQUE KEY `UNIQUE` (`Mail`,`Telephone`);
 
 --
 -- Indici per le tabelle `usrhubs`
 --
 ALTER TABLE `usrhubs`
-  ADD PRIMARY KEY (`ID-U`),
-  ADD KEY `Usr_Hub` (`ID-H`);
+  ADD PRIMARY KEY (`ID_U`),
+  ADD KEY `Usr_Hub` (`ID_H`);
 
 --
 -- Indici per le tabelle `wagons`
 --
 ALTER TABLE `wagons`
-  ADD PRIMARY KEY (`ID-W`);
+  ADD PRIMARY KEY (`ID_W`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
@@ -227,43 +227,43 @@ ALTER TABLE `wagons`
 -- AUTO_INCREMENT per la tabella `boxes`
 --
 ALTER TABLE `boxes`
-  MODIFY `ID-B` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_B` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `entersprises`
 --
 ALTER TABLE `entersprises`
-  MODIFY `ID-U` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_U` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `hubs`
 --
 ALTER TABLE `hubs`
-  MODIFY `ID-H` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_H` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `privates`
 --
 ALTER TABLE `privates`
-  MODIFY `ID-U` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_U` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT per la tabella `trains`
 --
 ALTER TABLE `trains`
-  MODIFY `ID-T` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_T` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID-U` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_U` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT per la tabella `wagons`
 --
 ALTER TABLE `wagons`
-  MODIFY `ID-W` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_W` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Limiti per le tabelle scaricate
@@ -273,41 +273,41 @@ ALTER TABLE `wagons`
 -- Limiti per la tabella `boxes`
 --
 ALTER TABLE `boxes`
-  ADD CONSTRAINT `Box_Use` FOREIGN KEY (`ID-U`) REFERENCES `users` (`ID-U`),
-  ADD CONSTRAINT `Box_Wag` FOREIGN KEY (`ID-W`) REFERENCES `wagons` (`ID-W`);
+  ADD CONSTRAINT `Box_Use` FOREIGN KEY (`ID_U`) REFERENCES `users` (`ID_U`),
+  ADD CONSTRAINT `Box_Wag` FOREIGN KEY (`ID_W`) REFERENCES `wagons` (`ID_W`);
 
 --
 -- Limiti per la tabella `entersprises`
 --
 ALTER TABLE `entersprises`
-  ADD CONSTRAINT `Ent_Use` FOREIGN KEY (`ID-U`) REFERENCES `users` (`ID-U`);
+  ADD CONSTRAINT `Ent_Use` FOREIGN KEY (`ID_U`) REFERENCES `users` (`ID_U`);
 
 --
 -- Limiti per la tabella `has`
 --
 ALTER TABLE `has`
-  ADD CONSTRAINT `Has_Tra` FOREIGN KEY (`ID-T`) REFERENCES `trains` (`ID-T`),
-  ADD CONSTRAINT `Has_Wag` FOREIGN KEY (`ID-W`) REFERENCES `wagons` (`ID-W`);
+  ADD CONSTRAINT `Has_Tra` FOREIGN KEY (`ID_T`) REFERENCES `trains` (`ID_T`),
+  ADD CONSTRAINT `Has_Wag` FOREIGN KEY (`ID_W`) REFERENCES `wagons` (`ID_W`);
 
 --
 -- Limiti per la tabella `passes_by`
 --
 ALTER TABLE `passes_by`
-  ADD CONSTRAINT `Pas_Hub` FOREIGN KEY (`ID-H`) REFERENCES `users` (`ID-U`),
-  ADD CONSTRAINT `Pas_Tra` FOREIGN KEY (`ID-T`) REFERENCES `trains` (`ID-T`);
+  ADD CONSTRAINT `Pas_Hub` FOREIGN KEY (`ID_H`) REFERENCES `hubs` (`ID_H`),
+  ADD CONSTRAINT `Pas_Tra` FOREIGN KEY (`ID_T`) REFERENCES `trains` (`ID_T`);
 
 --
 -- Limiti per la tabella `privates`
 --
 ALTER TABLE `privates`
-  ADD CONSTRAINT `Pri_Use` FOREIGN KEY (`ID-U`) REFERENCES `users` (`ID-U`);
+  ADD CONSTRAINT `Pri_Use` FOREIGN KEY (`ID_U`) REFERENCES `users` (`ID_U`);
 
 --
 -- Limiti per la tabella `usrhubs`
 --
 ALTER TABLE `usrhubs`
-  ADD CONSTRAINT `Usr_Hub` FOREIGN KEY (`ID-H`) REFERENCES `hubs` (`ID-H`),
-  ADD CONSTRAINT `Usr_Use` FOREIGN KEY (`ID-U`) REFERENCES `users` (`ID-U`);
+  ADD CONSTRAINT `Usr_Hub` FOREIGN KEY (`ID_H`) REFERENCES `hubs` (`ID_H`),
+  ADD CONSTRAINT `Usr_Use` FOREIGN KEY (`ID_U`) REFERENCES `users` (`ID_U`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
